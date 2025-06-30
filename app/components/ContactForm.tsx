62944ea3b2c9b2931dccd4b6544313243e9d35f0
jdifek <givotkov11@gmail.com>
2025-06-30 20:33:39 +0300

    wqwqw

--- app/components/ContactForm.tsx
+++ app/components/ContactForm.tsx
@@ -27,7 +27,7 @@ export const ContactForm: React.FC = () => {
     }
   
     try {
-      const response = await fetch('http://localhost:3002/form', {
+      const response = await fetch('https://ra-agency-back-production-246f.up.railway.app/form', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
