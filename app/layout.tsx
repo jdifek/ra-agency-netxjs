6b1eb09865190dddddc9c48a984d21bc1bb89335
jdifek <givotkov11@gmail.com>
2025-07-30 13:30:46 +0200

    wqwqwq

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,16 @@
+'use client';
+
 import { ReactNode } from 'react';
+import { ThemeProvider } from 'next-themes';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-  return children;
+  return (
+    <html lang="en">
+      <body>
+        <ThemeProvider attribute="class">
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
 }
